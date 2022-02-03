7301e3fd38b61a7320c5453b677479f3535caaf1
“reyshazni” <reyshazni@gmail.com>
2022-02-03 10:16:47 +0700

    update

--- Employee.java
+++ Employee.java
@@ -18,6 +18,7 @@ public class Employee {
     public Employee(String name, double baseSalary) {
         this.name = name;
         this.baseSalary = baseSalary;
+        employeeCount += 1;
     }
 
     /**
