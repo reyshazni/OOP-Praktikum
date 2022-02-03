ea33855d7533ebe605dbc46da57b01a8d2c7c13d
“reyshazni” <reyshazni@gmail.com>
2022-02-03 11:21:39 +0700

    update

--- Praktikum 1/Main.java
+++ Praktikum 1/Main.java
@@ -1,6 +1,7 @@
 /**
  * Main.java
- * [jelaskan kegunaan file ini]
+ * [Membaca input berupa name, baseSalary, currentPoints, dan targetPoints lalu mengeluarkan output berupa apakah employee promotable
+ *  dan apabila iya, akan mengeluarkan jumlah salary, apabila tidak, akan mengeluarkan point yang dibutuhkan agar bisa promosi]
  * @author [18220013] [Muhammad Rey Shazni Helmi]
  */
 import java.lang.System;
@@ -8,6 +9,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        double baseSalary = scanner.nextDouble();
+        double currentPoints = scanner.nextDouble();
+        double targetPoints = scanner.nextDouble();
         
+        Employee employee = new Employee(name, baseSalary);
+        EmployeePerformance employeeperformance = new EmployeePerformance(employee, currentPoints, targetPoints);
+
+        if (employeeperformance.isPromotable()) {
+            System.out.println("YES");
+            System.out.printf("%.2f\n", employeeperformance.getCurrentSalary());
+        } else {
+            System.out.println("NO");   
+            System.out.printf("%.2f\n", 2*targetPoints - currentPoints);         
+        }
+
     }
 }
