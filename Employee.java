a833c87f91877b256d1069e278e1c422cca405bc
Rey Shazni <69889187+reyshazni@users.noreply.github.com>
2022-02-03 10:02:47 +0700

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,52 @@
+/**
+ * Employee.java
+ * [Jelaskan kegunaan class ini]
+ * @author [NIM] [Nama]
+ */
+
+public class Employee {
+    private static int employeeCount;
+
+    private String name;
+    private double baseSalary;
+
+    /**
+     * Konstruktor
+     * @param name
+     * @param baseSalary
+     */
+    public Employee(String name, double baseSalary) {
+
+    }
+
+    /**
+     * Menambahkan gaji pegawai
+     * @param amount jumlah peningkatan, asumsi amount >= 0
+     */
+    public void increaseSalary(double amount) {
+
+    }
+
+    /**
+     * Getter name
+     * @return name
+     */
+    public String getName() {
+
+    }
+
+    /**
+     * Getter baseSalary
+     * @return baseSalary
+     */
+    public double getBaseSalary() {
+
+    }
+
+    /**
+     * @return jumlah karyawan
+     */
+    public static int getEmployeeCount() {
+
+    }
+}
