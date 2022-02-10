36c05809606a7a5799697e57a6f51900f5ff6c62
“reyshazni” <reyshazni@gmail.com>
2022-02-10 10:57:03 +0700

    update

---
+++ Praktikum 2/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main.java
+ * [jelaskan kegunaan file ini]
+ * @author [18220013] [Muhammad Rey Shazni Helmi]
+ */
+import java.lang.System;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
