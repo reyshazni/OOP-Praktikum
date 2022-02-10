243ee94072be691c7b2cc5c172374602e6b48fe3
“reyshazni” <reyshazni@gmail.com>
2022-02-10 10:57:08 +0700

    update

---
+++ Praktikum 2/Car.java
@@ -0,0 +1,40 @@
+/**
+ * Car.java
+ * [Kelas-kelas mobil untuk memodelkan mobil yang akan dijual oleh perusahaan Tasle]
+ * @author [18220013] [Muhammad Rey Shazni Helmi]
+ */
+
+public abstract class Car {
+    private int numberOfWheels;
+    private long mileage;
+    
+    public Car(int numberOfWheels) {
+        // Konstruktor, set mileage = 0
+        this.numberOfWheels = numberOfWheels;
+        this.mileage = 0;
+    }
+    
+    public int getNumberOfWheels() {
+        // Mengembalikan atribut numberOfWheels 
+        return numberOfWheels;
+    }
+  
+    public long getMileage() {
+        // Mengembalikan atribut mileage
+        return mileage;
+    }
+    
+    public void increaseMileage(long increment) {
+        // Meningkatkan nilai atribut mileage
+        this.mileage += increment;
+    }
+  
+    public String toString() {
+        String newStr = String.format("Number of Wheels: %d, Mileage: &d", numberOfWheels, mileage);
+        // Mengembalikan informasi mobil dengan format: "Number of Wheels: {numberOfWheels}, Mileage: {mileage}"
+        return newStr;
+    }
+  
+    public abstract long getPrice();
+}
+  
